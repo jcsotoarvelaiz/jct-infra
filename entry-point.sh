badd050e164f6595177c7c3c6d7283ddf9a8b631
Juan Soto <jcsotoarvelaiz@gmail.com>
2021-02-17 16:27:39 -0500

    Adding basic buildspec

--- entry-point.sh
+++ entry-point.sh
@@ -1,5 +1,4 @@
-apt-get install curl
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscliv2.zip"
+wget -O awscliv2.zip https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip
 unzip awscliv2.zip
 ./aws/install
 aws s3 cp JCT_STATE_LOCATION .
