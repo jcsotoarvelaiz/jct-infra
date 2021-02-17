b941441831ce6d4b97490b85321f0c571f7b989f
Juan Soto <jcsotoarvelaiz@gmail.com>
2021-02-17 13:06:35 -0500

    adding basic template

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+	required_version = ">= 0.12"
+}
+
+provider "aws" {
+	region = "us-east-1"
+}
+
+resource "aws_instance" "foobar" {
+	ami = "ami-047a51fa27710816e"
+	instance_type = "t1.micro"
+}
