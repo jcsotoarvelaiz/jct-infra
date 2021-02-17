3b7e396102ce7a39bf7a521307633afbcd2ac8e1
Juan Soto <jcsotoarvelaiz@gmail.com>
2021-02-17 16:32:20 -0500

    Adding basic buildspec

--- entry-point.sh
+++ entry-point.sh
@@ -1,9 +1,9 @@
 wget -O awscliv2.zip https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip
 unzip awscliv2.zip
-./aws/install
+aws/install
 aws s3 cp JCT_STATE_LOCATION .
-terraform version
-terraform init
-terraform apply -auto-approve
+#terraform version
+#terraform init
+#terraform apply -auto-approve
 env
 aws s3 cp terraform.tfstate $JCT_STATE_LOCATION
