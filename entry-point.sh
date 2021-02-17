9ca07dc8d01e7e60cb015ed4f5a60b926eb5c232
Juan Soto <jcsotoarvelaiz@gmail.com>
2021-02-17 16:10:14 -0500

    Adding basic buildspec

--- entry-point.sh
+++ entry-point.sh
@@ -1,3 +1,6 @@
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
 aws s3 cp JCT_STATE_LOCATION .
 terraform version
 terraform init
