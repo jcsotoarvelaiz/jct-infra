ccf0cf8dc8c10725255d328316e87eb6b277d0d2
Juan Soto <jcsotoarvelaiz@gmail.com>
2021-02-19 14:55:46 -0500

    Adding support for terraform destroy

--- entry-point.sh
+++ entry-point.sh
@@ -9,10 +9,10 @@ fi
 
 terraform version
 terraform init
-if [ "$JCT_TERRAFORM_OPERATION" == 'apply' ]; then
+if [ "$JCT_TERRAFORM_OPERATION" = 'apply' ]; then
    terraform apply -auto-approve
 fi
-if [ "$JCT_TERRAFORM_OPERATION" == 'destroy' ]; then
+if [ "$JCT_TERRAFORM_OPERATION" = 'destroy' ]; then
    terraform destroy -auto-approve
 fi
 aws s3 cp terraform.tfstate s3://$JCT_STATE_BUCKET/$JCT_STATE_KEY
